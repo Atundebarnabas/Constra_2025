6887c4f6cb262177346d30465a153ad404a4ce2c
barnabas02005 <barnabasatundej@gmail.com>
2025-06-14 14:05:21 +0100

    Update main.py

--- take_trade_bot/main.py
+++ take_trade_bot/main.py
@@ -31,7 +31,7 @@ db_conn = Database(
     user= os.getenv('DB_USER'),
     password= os.getenv('DB_PASSWORD'),
     database= os.getenv('DB_DATABASE'),
-    port= os.getenv('DB_PORT')
+    port= int(os.getenv('DB_PORT'))
 )
 
 def ensure_user_cred_table_exists():
