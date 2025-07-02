d35fc561b2c86bac4fc907871e734bf828f0930f
Atundebarnabas <89323794+Atundebarnabas@users.noreply.github.com>
2025-07-02 18:40:54 +0100

    Update main.py

--- trade_management/main.py
+++ trade_management/main.py
@@ -591,6 +591,7 @@ def monitor_position_and_reenter(exchange, trade_id, symbol, position, lv_size, 
             raw_size = contracts / 4
 
         # Adjust re-entry size according to amount precision
+        re_entry_size = adjust_size_to_precision(raw_size, amount_precision)
         re_entry_size = normalize_amount(re_entry_size, precision['amount'])
         
         if verbose:
